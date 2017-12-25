db17b4a7d398f0c1e3b80a523956585676ce2bc5
Augusto Falcao <falcaoaugustos@gmail.com>
2017-12-25 15:42:21 -0300

    feat (routes): create root route

--- routes/app.js
+++ routes/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
-
 const beers = require('./beers-route')
+
+app.get('/', (req, res) => {
+  res.send('Welcome to beers collection!')
+})
 
 app.use('/beers', beers)
 
